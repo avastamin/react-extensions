ca2778c15f9fc910001b56e41d5adcf4261b86ee
avastamin <r_amin105@yahoo.com>
2022-01-23 22:18:57 +0600

    Add toggle buttons and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import HomeLogo from './img/HomeLogo.svg';
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="container max-w-md">
+      <header className='bg-gray-700 p-4 border-b-2 border-gray-50'>
+        <img className='w-105 mr-auto ml-auto' src={HomeLogo} alt="Logo" />
+      </header>
+      <div className='p-6'>
+        <div className='font-Manrope flex flex-row justify-center items-center'>
+          <button className='w-66 py-1.5 px-3 border border-gray-500 bg-gray-800 rounded-xl mr-4'>Email</button>
+          <button className='w-66 py-1.5 px-3 border border-gray-500 bg-gray-800 rounded-xl'>Phone</button>
+        </div>
+        <div className='p-6 text-center'>
+          <form>
+            <input type="text" 
+              className='w-full py-1.5 px-3 border border-gray-500 bg-gray-800 rounded-md mr-4' 
+              placeholder="Ex (337) 378 8383" 
+            />
+            <button className='h-22 py-2 pr-4 pl-7 mt-5 mr-auto ml-auto text-white bg-gray-500 border border-gray-500 bg-gray-800 rounded-lg mr-4'>Continue</button>
+          </form>
+        </div>
+        <div className='text-center mt-4'>
+          <p className='text-gray-200 text-sm'>by clicking continue you must agree to near labs 
+            <a href='#' className='text-blue-100'> Terms & Conditions ans Privacy Policy</a></p>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,43 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+    },
+    container: {
+      center: true,
+      maxWidth: {
+        'md': '700px'
+      }
+    },
+    colors: {
+      'white': '#FFFFFF',
+      'blue':{
+        100: '#587BE0',
+      },
+      'gray': {
+        50: '#DFDFE0',
+        200: '#414047',
+        300: '6F6E73',
+        500: '#BEBEC2',
+        700: '#f5f5f5',
+        800: '#FCFCFC',
+      },
+      /* 'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6', */
+    },
+    extend: {
+      fontFamily: {
+        Manrope: ['Manrope', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
