218d1fac355fbb4ef7234b01caa3325bb2f58448
avastamin <r_amin105@yahoo.com>
2022-01-24 08:43:37 +0600

    Add login with NEAR section

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,17 @@ function App() {
               className='w-full py-1.5 px-3 border border-gray-500 bg-gray-800 rounded-md mr-4' 
               placeholder="Ex (337) 378 8383" 
             />
-            <button className='h-22 py-2 pr-4 pl-7 mt-5 mr-auto ml-auto text-white bg-gray-500 border border-gray-500 bg-gray-800 rounded-lg mr-4'>Continue</button>
+            <button className='h-22 py-2 pr-4 pl-7 mt-5 mr-auto ml-auto text-white bg-gray-500 border border-gray-500 rounded-lg mr-4'>Continue</button>
           </form>
         </div>
         <div className='text-center mt-4'>
           <p className='text-gray-200 text-sm'>by clicking continue you must agree to near labs 
             <a href='#' className='text-blue-100'> Terms & Conditions ans Privacy Policy</a></p>
+        </div>
+        <div className='w-full h-px my-8 bg-gray-500'></div>
+        <div className='text-center mt-4'>
+          <p>Already have NEAR account?</p>
+          <button className='h-22 py-2 pr-4 pl-7 mt-2 mr-auto ml-auto text-white bg-gray-200 border border-gray-500 rounded-lg mr-4'>Login in with NEAR</button>
         </div>
       </div>
     </div>
